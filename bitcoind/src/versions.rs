b720056c6cdae7b9945e40adaa96c1275bb0af25
xyzconstant <263061129+xyzconstant@users.noreply.github.com>
2026-05-06 13:09:22 -0300

    bitcoind: add support for Bitcoin Core 31.0 release

--- bitcoind/src/versions.rs
+++ bitcoind/src/versions.rs
@@ -1,6 +1,7 @@
 // An explicit version of Bitcoin Core must be selected by enabling some feature.
 // We check this here instead of in `lib.rs` because this file is included in `build.rs`.
 #[cfg(all(
+    not(feature = "31_0"),
     not(feature = "30_2"),
     not(feature = "30_0"),
     not(feature = "29_0"),
@@ -23,8 +24,11 @@
 ))]
 compile_error!("enable a feature in order to select the version of Bitcoin Core to use");
 
-#[cfg(feature = "30_2")]
+#[cfg(feature = "31_0")]
 #[allow(dead_code)] // Triggers in --all-features builds.
+pub const VERSION: &str = "31.0";
+
+#[cfg(all(feature = "30_2", not(feature = "31_0")))]
 pub const VERSION: &str = "30.2";
 
 #[cfg(all(feature = "30_0", not(feature = "30_2")))]
@@ -84,6 +88,8 @@ pub const VERSION: &str = "0.17.2";
 /// This is meaningless but we need it otherwise we can't get far enough into
 /// the build process to trigger the `compile_error!` in `./versions.rs`.
 #[cfg(all(
+    not(feature = "31_0"),
+    not(feature = "30_2"),
     not(feature = "30_0"),
     not(feature = "29_0"),
     not(feature = "28_2"),
